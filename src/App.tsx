b10c9249243b91899daf6acfaa78bb3ead7c3230
Emerson Melo Martins <emerson25xd@gmail.com>
2022-10-13 20:29:23 -0300

    radix, tailwindcss, storybook to doc components, clsx, phosphor-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import "./styles/global.css";
+
 export function App() {
-  return <h1>App</h1>;
+  return (
+  
+  <>
+  <h1 className="font-bold text-2xl text-violet-500">App</h1>
+  
+  <button className="bg-cyan-500 font-medium px-5 py-2 rounded text-white hover:bg-cyan-300">Botão</button>
+  </>
+  
+  );
 }
