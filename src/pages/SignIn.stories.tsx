1291b204855409328c2ffa7bce894ca3ff1d57a3
Emerson Melo Martins <emerson25xd@gmail.com>
2022-10-14 18:48:25 -0300

    Testing page with storybook

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { SignIn } from "./SignIn";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
+export default {
+  title: "Pages/Sign in",
+  component: SignIn,
+  args: {},
+  argTypes: {},
+} as Meta;
+
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Digite seu e-mail"),
+      "emerson25xd@gmail.com"
+    );
+    userEvent.type(canvas.getByPlaceholderText("******"), "123456");
+
+    userEvent.click(canvas.getByRole("button"));
+
+    await waitFor(() => {
+      return expect(canvas.getByText("Login realizado!")).toBeInTheDocument();
+    });
+  },
+};
